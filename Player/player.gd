309105192b38ba9ad965af29ec11d3c76d68f8b2
Omegaz102 <130893707+Omegaz102@users.noreply.github.com>
2026-01-27 14:51:03 -0800

    Added good larp based movement, has sprinting
    Needs polish and jummping(I will do this)

--- Player/player.gd
+++ Player/player.gd
@@ -1,15 +1,16 @@
 extends CharacterBody3D
 
 var sens = 0.3	#mouse sensitivity
-const minSpeed = 5 #minimum speed while walking/running
+const minSpeed = 3 #minimum speed while walking/running
 const maxSpeed = 6.7 #67
 const maxRunSpeed = 4.1 #41
-const walkAcceleration = 20
+const walkAcceleration = 2
 const runAcceleration = 25
 const deceleration = 30	#velocity is divided by 1 + deceleration when decelerating
 var acceleration = 30 	#how much to accelerate
 var stamina = 100 #I don't think this needs explaination
-var running = 0 # 1 or 0 for is running, not bool because I can avoid an if statement if by making it a number
+var running = false # 1 or 0 for is running, not bool because I can avoid an if statement if by making it a number
+const larp = 30 # Speed of lerping between velocity
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -33,37 +34,25 @@ func _physics_process(delta: float) -> void:
 	
 	
 	if Input.is_action_just_pressed("run") and stamina >= 0:
-		running = 1
+		running = true
 	if Input.is_action_just_released("run") or stamina <= 0:
-		running = 0 
+		running = false
 	
-	stamina -= 10 * delta * running
-	stamina += 2 * delta * (1 - running)
+	stamina -= 40 * delta * int(running)
+	stamina += 40 * delta * (1 - int(running))
 	
 	stamina = clamp(stamina, 0, 100)
 
 	%StaminaMeater.frame = remap(stamina, 100, 0, 0, 60)
 	
-#	if running: #make adams code more comprehensible -- LEGACY CODE
-#		acceleration = float(inputVector2D.length()) * runAcceleration
-#	else:
-#		acceleration = float(inputVector2D.length()) * walkAcceleration
-#	
-#	if velocity.length() > inputVector2D.length() * (maxSpeed + running * maxRunSpeed): # nice lerp velovity thing
-#		velocity = velocity.lerp(Vector3.ZERO, deceleration * delta)
-#	velocity += inputVector3D * acceleration * delta
-	
 	var planarVelocity = Vector2(velocity.x, velocity.z) #seperates planar movment so planar movment doesn't affect jumping/falling
-	%FPSDisplay.text = str(planarVelocity.length()) # using fps meeter for live testing
+	var targetVelocity = Vector2() #planar velocity sans lerp
 	if inputVector2D.length() > 0: #new system for handling movement
-		if planarVelocity.length() <= minSpeed:
-			planarVelocity = Vector2(inputVector3D.x, inputVector3D.z) * minSpeed
-		else:
-			planarVelocity = (planarVelocity.length() + acceleration*delta) * Vector2(inputVector3D.x, inputVector3D.z)
+		targetVelocity = (maxSpeed + maxRunSpeed * int(running)) * Vector2(inputVector3D.x, inputVector3D.z)  # target speed get's what will be lerped to
+	
 	else:
-		planarVelocity = planarVelocity.normalized() * (planarVelocity.length() - deceleration * delta)
-	
-	
+		targetVelocity = Vector2()
+	planarVelocity = lerp(planarVelocity, targetVelocity, delta * larp)
 	velocity = Vector3(planarVelocity.x, velocity.y, planarVelocity.y)
 	
 	move_and_slide()
