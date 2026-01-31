642610d2682428ba3ca5ca3fa59ae1d0f9596283
Omegaz102 <Adamzacher@outlook.com>
2026-01-31 11:42:24 -0800

    Added loadScene func to main

--- godot/ecovally/main.gd
+++ godot/ecovally/main.gd
@@ -1,4 +1,15 @@
 extends Node
+var currentScene: Node
+
+func _ready() -> void:
+	loadScene("res://test_world.tscn")
+
+func loadScene(pathToScene: String):
+	if currentScene != null:
+		currentScene.queue_free()
+	var newScene: PackedScene = load(pathToScene)
+	currentScene = newScene.instantiate()
+	add_child(currentScene)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
