b099d116710afbf2a32f79f2757b8a5d95cff984
Omegaz102 <Adamzacher@outlook.com>
2026-01-31 18:34:35 -0800

    fixed scroll up bug

--- godot/ecovally/Player/hot_bar.gd
+++ godot/ecovally/Player/hot_bar.gd
@@ -30,11 +30,12 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("NextItem"):
 		selected += 1
 		if selected > hotBar.size() - 1:
-			selected = 1
+			selected = 0
 		if hotBar[selected] != null:
 			changeItem(hotBar[selected])
 		else:
-			remove_child(get_child(0))
+			if get_child_count() > 0:
+				remove_child(get_child(0))
 			
 	if Input.is_action_just_pressed("PreviousItem"):
 		selected -= 1
@@ -43,4 +44,6 @@ func _physics_process(delta: float) -> void:
 		if hotBar[selected] != null:
 			changeItem(hotBar[selected])
 		else:
-			remove_child(get_child(0))
+			if get_child_count() > 0:
+				remove_child(get_child(0))
+	%DevLable.text = str(selected)
