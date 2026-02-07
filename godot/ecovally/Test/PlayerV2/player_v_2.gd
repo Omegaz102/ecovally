6f6d8f67cb5a8c298c113eb4bf774f7f8a3a4fe4
Omegaz102 <Adamzacher@outlook.com>
2026-02-07 14:31:53 -0800

    Deleted dialogic because it is stupid

--- godot/ecovally/Test/PlayerV2/player_v_2.gd
+++ godot/ecovally/Test/PlayerV2/player_v_2.gd
@@ -1,24 +1,29 @@
-extends RigidBody3D
+extends CharacterBody3D
 var sens: float = 0.3
-var maxSpeed: float = 20
-var speed: float = 20
+var maxSpeed: float = 10
+var speed: float = 40
+var friction = 0.4
+@export var movementCurve: Curve 
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:	#mouse input
-		$Node3D.rotation_degrees.y -= event.relative.x * sens
+		rotation_degrees.y -= event.relative.x * sens
 		%Camera3D.rotation_degrees.x -= event.relative.y * sens
 		%Camera3D.rotation_degrees.x = clamp(%Camera3D.rotation_degrees.x, -80, 80)
 
 func _physics_process(delta: float) -> void:
+	var planarVelocity = Vector2(velocity.x, velocity.z)
 	var inputVector2D = Input.get_vector("left", "right", "forward", "backward")
 	inputVector2D = inputVector2D.normalized()
 	var inputVector3D = Vector3(
 		inputVector2D.x, 0, inputVector2D.y
 	)
-	inputVector3D = $Node3D.transform.basis * inputVector3D
+	inputVector3D = transform.basis * inputVector3D
+	var planarInput = Vector2(inputVector3D.x, inputVector3D.z)
 	
-	linear_velocity += inputVector3D * (maxSpeed - Vector2(axis_lock_linear_x,linear_velocity.z).length()) / maxSpeed * speed * delta
+	planarVelocity = planarVelocity.normalized() * planarVelocity.length() * (planarVelocity.dot(planarInput)) 
 	
-	if Input.is_action_just_pressed("jump") and $RayCast3D.is_colliding():
-		linear_velocity.y += 5
-		
+	move_and_slide()
