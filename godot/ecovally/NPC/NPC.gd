6b230692a922225f45b8c6c2d7a9ba2f87f5617c
Omegaz102 <Adamzacher@outlook.com>
2026-09-08 21:40:47 -0700

    worked more on dialogue system

--- godot/ecovally/NPC/NPC.gd
+++ godot/ecovally/NPC/NPC.gd
@@ -3,10 +3,19 @@ extends CharacterBody3D
 
 signal Interacted()
 @export var DisplayName = "[NPC]"
+@export var idleText:Array[String] = ["sup"]
+var speachQueue:Dictionary = {"funFact":"Fun fact, you can interact with the cube to my left. Give it a spin and come back to me when you're done."}
+
+signal spoke(textID)
 
 func interact():
 	emit_signal("Interacted")
-	say("hello")
+	if speachQueue.size() == 0:
+		say(idleText.pick_random())
+	else:
+		say(speachQueue.values()[0])
+		emit_signal("spoke", speachQueue.keys()[0])
+		print(speachQueue.erase(speachQueue.keys()[0]))
 	
 func say(text):
 	print(text)
